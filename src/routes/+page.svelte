a35dbf903ff90fd20cd031343a4249f67828649e
Kawsar Ahmed <prokawsar@gmail.com>
2023-09-20 08:59:37 +0600

    title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,15 +31,17 @@
 </svelte:head>
 
 <div class="container mx-auto mt-20 flex flex-col items-center gap-5 justify-center">
+	<p class="font-bold text-3xl">Kinde Auth</p>
+	<p class="text-sm text-gray-400">@kinde-oss/kinde-sveltekit-sdk v1.2.0</p>
 	<div class="w-60 flex flex-col gap-5 items-center">
-		<div class="flex flex-row gap-3">
+		<div class="flex flex-row gap-3 w-full">
 			<Button
-				classes="w-full outline-gray-300"
+				classes="w-full !outline-violet-600"
 				text="Login"
 				onClick={() => goto('/api/auth/login')}
 			/>
 			<Button
-				classes="w-full outline-green-300"
+				classes="w-full !outline-green-500"
 				text="Register"
 				onClick={() => goto('/api/auth/register')}
 			/>
