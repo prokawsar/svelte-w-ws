17717eb9630bc8fbe121fdb6f842db783af35dd9
Kawsar Ahmed <prokawsar@gmail.com>
2023-09-20 15:58:59 +0600

    session hooks

---
+++ src/hooks.server.ts
@@ -0,0 +1,8 @@
+// src/hooks.server.ts
+import { sessionHooks, type Handler } from '@kinde-oss/kinde-sveltekit-sdk';
+
+export const handle: Handler = async ({ event, resolve }) => {
+	sessionHooks({ event });
+	const response = await resolve(event);
+	return response;
+};
