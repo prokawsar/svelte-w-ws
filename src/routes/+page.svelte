227308e9b8c7ebe6a9a7356722d3c6acad1b24d0
Kawsar Ahmed <prokawsar@gmail.com>
2023-09-21 10:50:41 +0600

    kinde sdk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,31 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import Button from '$lib/elements/button.svelte';
-	import Count from '$lib/elements/count.svelte';
-	import { count } from '$lib/store/store';
-	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation'
+	import Button from '$lib/elements/button.svelte'
+	import Count from '$lib/elements/count.svelte'
+	import { count } from '$lib/store/store'
+	import { onMount } from 'svelte'
 
-	const ws_url = 'ws://localhost:8080';
-	let ws: WebSocket;
+	export let data
+
+	const ws_url = 'ws://localhost:8080'
+	let ws: WebSocket
 
 	onMount(() => {
-		ws = new WebSocket(ws_url);
+		ws = new WebSocket(ws_url)
 		ws.onmessage = (event) => {
-			const data = JSON.parse(event.data);
+			const data = JSON.parse(event.data)
 			if (data.type === 'count') {
-				$count = data.count;
+				$count = data.count
 			}
-		};
-	});
+		}
+	})
 
 	const incrementCounter = () => {
-		ws.send('increment');
-	};
+		ws.send('increment')
+	}
 	const resetCounter = () => {
-		ws.send('reset');
-	};
+		ws.send('reset')
+	}
 </script>
 
 <svelte:head>
@@ -31,10 +33,18 @@
 </svelte:head>
 
 <div class="container mx-auto mt-20 flex flex-col items-center gap-5 justify-center">
-	<p class="font-bold text-3xl">Kinde Auth</p>
+	<p class="font-bold text-3xl">
+		K<span class="uppercase text-lg">inde</span> A<span class="uppercase text-lg">uth</span>
+	</p>
 	<p class="text-sm text-gray-400">@kinde-oss/kinde-sveltekit-sdk v1.2.0</p>
+
+	<p class="font-bold text-3xl">
+		A<span class="uppercase text-lg">thenticated</span>
+		<span class="uppercase text-lg">{data.isAuthenticated}</span>
+	</p>
+
 	<div class="w-60 flex flex-col gap-5 items-center">
-		<div class="flex flex-row gap-3 w-full">
+		<div class="flex flex-row gap-3 w-full justify-center">
 			<Button
 				classes="w-full !outline-violet-600"
 				text="Login"
--- src/routes/api/auth/[...kindeAuth]/+server.ts
+++ src/routes/api/auth/[...kindeAuth]/+server.ts
@@ -1,5 +1,6 @@
 import { handleAuth } from '@kinde-oss/kinde-sveltekit-sdk';
+import type { RequestEvent } from '@sveltejs/kit';
 
-export function GET(requestEvents) {
+export function GET(requestEvents: RequestEvent) {
 	return handleAuth(requestEvents);
 }
