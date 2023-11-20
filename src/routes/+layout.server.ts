90b62ec019370cee009e67a1664ef3b80c20ba29
Kawsar Ahmed <prokawsar@gmail.com>
2023-11-20 11:10:33 +0600

    test updated

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,16 +1,11 @@
 import {
-	KINDE_CLIENT_ID,
-	KINDE_CLIENT_SECRET,
-	KINDE_ISSUER_URL,
-	KINDE_REDIRECT_URL
-} from '$env/static/private'
-import {
 	getConfiguration,
 	kindeAuthClient,
 	UsersApi,
 	type SessionManager,
 	createKindeServerClient,
-	GrantType
+	GrantType,
+	OrganizationsApi
 } from '@kinde-oss/kinde-sveltekit-sdk'
 import type { RequestEvent } from '@sveltejs/kit'
 
@@ -35,37 +30,25 @@ export async function load({ request }: RequestEvent) {
 		)
 
 		const accessToken = await kindeAuthClient.getToken(request as unknown as SessionManager)
-		const kindeManagementApi = createKindeServerClient(GrantType.CLIENT_CREDENTIALS, {
-			// hardcoded M2M type applications client id and secret
-			clientId: 'ee241ab6dcfa4cee94c0e79e2386b2a2',
-			clientSecret: 'Da7RurVjtNF9pmj3KZ2P68CE6bkdWEdNhDXP3KWJmHRxVsyO9u',
-			authDomain: KINDE_ISSUER_URL,
-			logoutRedirectURL: KINDE_REDIRECT_URL
-		})
-		const token = await kindeManagementApi.getToken(request as unknown as SessionManager)
-		console.log('is token same: ', accessToken == token) // this is showing true
 		const headers = {
 			Accept: 'application/json',
 			Authorization: `Bearer ${accessToken}`
 		}
-		let url = `https://prokawsar.kinde.com/api/v1/organizations/org_3c1bc91bc0f/users`
+		let url = `https://prokawsar.kinde.com/api/v1/organization/?code=org_3c1bc91bc0f`
 		// let url = `https://prokawsar.kinde.com/api/v1/users`
-
 		try {
-			let users = await fetch(url, {
+			let resp = await fetch(url, {
 				method: 'GET',
 				headers: headers
 			})
-				.then(function (res) {
-					return res.json()
-				})
-				.then(function (body) {
-					return body
-				})
-			console.log(users.errors)
-			// const config = await getConfiguration()
-			// const apiIns = new UsersApi(config)
-			// const users = await apiIns.getUsers()
+			let users = await resp.json()
+			console.log(users)
+			const config = await getConfiguration()
+			const apiIns = new UsersApi(config)
+			const allUsers = await apiIns.getUsers()
+			const orgApi = new OrganizationsApi(config)
+			// const orgs = await orgApi.getOrganization()
+			// const allOrg = await orgApi.getOrganizations()
 
 			const theme = await kindeAuthClient.getFlag(request as unknown as SessionManager, 'theme')
 			const enable_dark_theme = await kindeAuthClient.getBooleanFlag(
